097b37aba79a6a01e7732328d5eb810e90945133
FuzeHere <fairuzofficial7@gmail.com>
2025-12-05 10:40:02 +0800

    day3

---
+++ day3.js
@@ -0,0 +1,12 @@
+// Ini lebih baik dari pada menggunakan 3 variabel berbeda yang dapat 
+// banyak variasi variabel atau saling timpa jika namanya sama
+
+
+const dataDiri = {
+    name : 'Fairuz',
+    umur : 19,
+    lahir : 2006,
+    Pekerjaan : false
+};
+
+console.log(dataDiri.name + ' lahir pada tahun ' + dataDiri.lahir);
